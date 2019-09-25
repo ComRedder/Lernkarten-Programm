25629be01fdea48ea82761ebfac2e71de7ac27f9
Nick Lierenfeld <nick-lierenfeld@outlook.de>
2019-09-25 16:27:21 +0200

    View

--- Lernkartentrainer/Lernkartentrainer/View.Designer.cs
+++ Lernkartentrainer/Lernkartentrainer/View.Designer.cs
@@ -28,46 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.sidepanel = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.headerpanel = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.sidepanel.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // sidepanel
+            // 
+            this.sidepanel.BackColor = System.Drawing.Color.RoyalBlue;
+            this.sidepanel.Controls.Add(this.button1);
+            this.sidepanel.Controls.Add(this.panel1);
+            this.sidepanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.sidepanel.Location = new System.Drawing.Point(0, 0);
+            this.sidepanel.Name = "sidepanel";
+            this.sidepanel.Size = new System.Drawing.Size(176, 554);
+            this.sidepanel.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Gray;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(176, 69);
+            this.panel1.TabIndex = 2;
+            // 
+            // headerpanel
+            // 
+            this.headerpanel.BackColor = System.Drawing.Color.DarkRed;
+            this.headerpanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.headerpanel.Location = new System.Drawing.Point(176, 0);
+            this.headerpanel.Name = "headerpanel";
+            this.headerpanel.Size = new System.Drawing.Size(688, 69);
+            this.headerpanel.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(158, 203);
+            this.button1.AutoSize = true;
+            this.button1.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(3, 75);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(102, 65);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Lehrer";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(347, 203);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(102, 65);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Schüler";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button1.Size = new System.Drawing.Size(170, 51);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Anmelden";
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // View
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(631, 488);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(864, 554);
+            this.Controls.Add(this.headerpanel);
+            this.Controls.Add(this.sidepanel);
             this.Name = "View";
             this.Text = "Lernkartentrainer";
+            this.sidepanel.ResumeLayout(false);
+            this.sidepanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private System.Windows.Forms.Panel sidepanel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel headerpanel;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
     }
 }
 
