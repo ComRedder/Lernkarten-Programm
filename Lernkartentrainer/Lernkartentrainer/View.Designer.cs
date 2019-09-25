d629195e0529ef5919f3ffdf758926773128e482
Nick Lierenfeld <nick-lierenfeld@outlook.de>
2019-09-25 16:03:20 +0200

    Buttons

--- Lernkartentrainer/Lernkartentrainer/View.Designer.cs
+++ Lernkartentrainer/Lernkartentrainer/View.Designer.cs
@@ -28,15 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(158, 203);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 65);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Lehrer";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(347, 203);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 65);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Schüler";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // View
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Turquoise;
-            this.ClientSize = new System.Drawing.Size(1047, 672);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(631, 488);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Name = "View";
             this.Text = "Lernkartentrainer";
             this.ResumeLayout(false);
@@ -44,6 +65,9 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
