a6068dc8b555ca774a76837528a4614b6894326d
Nick Lierenfeld <nick-lierenfeld@outlook.de>
2019-09-25 15:56:16 +0200

    View

--- Lernkartentrainer/Lernkartentrainer/View.Designer.cs
+++ Lernkartentrainer/Lernkartentrainer/View.Designer.cs
@@ -34,7 +34,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Turquoise;
+            this.ClientSize = new System.Drawing.Size(1047, 672);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "View";
             this.Text = "Lernkartentrainer";
             this.ResumeLayout(false);
