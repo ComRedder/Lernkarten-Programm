0f924a5879ce204ff8096da61279e883190c8c94
Nick Lierenfeld <nick-lierenfeld@web.de>
2019-09-26 20:17:40 +0200

    View

--- Lernkartentrainer/Lernkartentrainer/View.Designer.cs
+++ Lernkartentrainer/Lernkartentrainer/View.Designer.cs
@@ -29,60 +29,65 @@
         private void InitializeComponent()
         {
             this.sidepanel = new System.Windows.Forms.Panel();
+            this.buttonLogin = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.headerpanel = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
             this.sidepanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // sidepanel
             // 
             this.sidepanel.BackColor = System.Drawing.Color.RoyalBlue;
-            this.sidepanel.Controls.Add(this.button1);
+            this.sidepanel.Controls.Add(this.buttonLogin);
             this.sidepanel.Controls.Add(this.panel1);
             this.sidepanel.Dock = System.Windows.Forms.DockStyle.Left;
             this.sidepanel.Location = new System.Drawing.Point(0, 0);
+            this.sidepanel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.sidepanel.Name = "sidepanel";
-            this.sidepanel.Size = new System.Drawing.Size(176, 554);
+            this.sidepanel.Size = new System.Drawing.Size(264, 852);
             this.sidepanel.TabIndex = 0;
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.AutoSize = true;
+            this.buttonLogin.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonLogin.ForeColor = System.Drawing.SystemColors.Control;
+            this.buttonLogin.Location = new System.Drawing.Point(4, 115);
+            this.buttonLogin.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(255, 78);
+            this.buttonLogin.TabIndex = 3;
+            this.buttonLogin.Text = "Anmelden";
+            this.buttonLogin.UseVisualStyleBackColor = false;
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.Gray;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(176, 69);
+            this.panel1.Size = new System.Drawing.Size(264, 106);
             this.panel1.TabIndex = 2;
             // 
             // headerpanel
             // 
             this.headerpanel.BackColor = System.Drawing.Color.DarkRed;
             this.headerpanel.Dock = System.Windows.Forms.DockStyle.Top;
-            this.headerpanel.Location = new System.Drawing.Point(176, 0);
+            this.headerpanel.Location = new System.Drawing.Point(264, 0);
+            this.headerpanel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.headerpanel.Name = "headerpanel";
-            this.headerpanel.Size = new System.Drawing.Size(688, 69);
+            this.headerpanel.Size = new System.Drawing.Size(1032, 106);
             this.headerpanel.TabIndex = 1;
-            // 
-            // button1
-            // 
-            this.button1.AutoSize = true;
-            this.button1.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.SystemColors.Control;
-            this.button1.Location = new System.Drawing.Point(3, 75);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(170, 51);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Anmelden";
-            this.button1.UseVisualStyleBackColor = false;
             // 
             // View
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(864, 554);
+            this.ClientSize = new System.Drawing.Size(1296, 852);
             this.Controls.Add(this.headerpanel);
             this.Controls.Add(this.sidepanel);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "View";
             this.Text = "Lernkartentrainer";
             this.sidepanel.ResumeLayout(false);
@@ -96,7 +101,7 @@
         private System.Windows.Forms.Panel sidepanel;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel headerpanel;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonLogin;
     }
 }
 
