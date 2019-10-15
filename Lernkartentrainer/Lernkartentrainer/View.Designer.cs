5c79255f1a7f844fd5203961c4dcd6db34968181
Redder <49811666+ComRedder@users.noreply.github.com>
2019-10-15 23:36:59 +0200

    a

--- Lernkartentrainer/Lernkartentrainer/View.Designer.cs
+++ Lernkartentrainer/Lernkartentrainer/View.Designer.cs
@@ -29,45 +29,21 @@
         private void InitializeComponent()
         {
             this.sidepanel = new System.Windows.Forms.Panel();
-            this.buttonLogin = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
             this.headerpanel = new System.Windows.Forms.Panel();
+            this.buttonLogin = new System.Windows.Forms.Button();
             this.sidepanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // sidepanel
             // 
-            this.sidepanel.BackColor = System.Drawing.Color.RoyalBlue;
+            this.sidepanel.BackColor = System.Drawing.Color.DimGray;
             this.sidepanel.Controls.Add(this.buttonLogin);
-            this.sidepanel.Controls.Add(this.panel1);
             this.sidepanel.Dock = System.Windows.Forms.DockStyle.Left;
             this.sidepanel.Location = new System.Drawing.Point(0, 0);
             this.sidepanel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.sidepanel.Name = "sidepanel";
-            this.sidepanel.Size = new System.Drawing.Size(264, 852);
+            this.sidepanel.Size = new System.Drawing.Size(264, 887);
             this.sidepanel.TabIndex = 0;
-            // 
-            // buttonLogin
-            // 
-            this.buttonLogin.AutoSize = true;
-            this.buttonLogin.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonLogin.ForeColor = System.Drawing.SystemColors.Control;
-            this.buttonLogin.Location = new System.Drawing.Point(4, 115);
-            this.buttonLogin.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.buttonLogin.Name = "buttonLogin";
-            this.buttonLogin.Size = new System.Drawing.Size(255, 78);
-            this.buttonLogin.TabIndex = 3;
-            this.buttonLogin.Text = "Anmelden";
-            this.buttonLogin.UseVisualStyleBackColor = false;
-            // 
-            // panel1
-            // 
-            this.panel1.BackColor = System.Drawing.Color.Gray;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(264, 106);
-            this.panel1.TabIndex = 2;
             // 
             // headerpanel
             // 
@@ -76,15 +52,30 @@
             this.headerpanel.Location = new System.Drawing.Point(264, 0);
             this.headerpanel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.headerpanel.Name = "headerpanel";
-            this.headerpanel.Size = new System.Drawing.Size(1032, 106);
+            this.headerpanel.Size = new System.Drawing.Size(1364, 111);
             this.headerpanel.TabIndex = 1;
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.AutoSize = true;
+            this.buttonLogin.FlatAppearance.BorderSize = 0;
+            this.buttonLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonLogin.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonLogin.ForeColor = System.Drawing.Color.White;
+            this.buttonLogin.Location = new System.Drawing.Point(0, 132);
+            this.buttonLogin.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(264, 78);
+            this.buttonLogin.TabIndex = 3;
+            this.buttonLogin.Text = "Anmelden";
+            this.buttonLogin.UseVisualStyleBackColor = false;
             // 
             // View
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1296, 852);
+            this.ClientSize = new System.Drawing.Size(1628, 887);
             this.Controls.Add(this.headerpanel);
             this.Controls.Add(this.sidepanel);
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
@@ -99,7 +90,6 @@
         #endregion
 
         private System.Windows.Forms.Panel sidepanel;
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel headerpanel;
         private System.Windows.Forms.Button buttonLogin;
     }
