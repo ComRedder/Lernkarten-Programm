a06f3f9c834bc5bf2578290b7b26009e4fbed3bd
Redder <49811666+ComRedder@users.noreply.github.com>
2019-10-16 01:47:31 +0200

    Buttons

--- Lernkartentrainer/Lernkartentrainer/View.Designer.cs
+++ Lernkartentrainer/Lernkartentrainer/View.Designer.cs
@@ -30,19 +30,29 @@
         {
             this.sidepanel = new System.Windows.Forms.Panel();
             this.headerpanel = new System.Windows.Forms.Panel();
-            this.buttonLogin = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
             this.sidepanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // sidepanel
             // 
-            this.sidepanel.BackColor = System.Drawing.Color.DimGray;
-            this.sidepanel.Controls.Add(this.buttonLogin);
+            this.sidepanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(39)))), ((int)(((byte)(40)))));
+            this.sidepanel.Controls.Add(this.button7);
+            this.sidepanel.Controls.Add(this.button6);
+            this.sidepanel.Controls.Add(this.button5);
+            this.sidepanel.Controls.Add(this.button4);
+            this.sidepanel.Controls.Add(this.button3);
+            this.sidepanel.Controls.Add(this.button2);
             this.sidepanel.Dock = System.Windows.Forms.DockStyle.Left;
             this.sidepanel.Location = new System.Drawing.Point(0, 0);
             this.sidepanel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.sidepanel.Name = "sidepanel";
-            this.sidepanel.Size = new System.Drawing.Size(264, 887);
+            this.sidepanel.Size = new System.Drawing.Size(264, 769);
             this.sidepanel.TabIndex = 0;
             // 
             // headerpanel
@@ -52,37 +62,99 @@
             this.headerpanel.Location = new System.Drawing.Point(264, 0);
             this.headerpanel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.headerpanel.Name = "headerpanel";
-            this.headerpanel.Size = new System.Drawing.Size(1364, 111);
+            this.headerpanel.Size = new System.Drawing.Size(1164, 111);
             this.headerpanel.TabIndex = 1;
             // 
-            // buttonLogin
+            // button2
             // 
-            this.buttonLogin.AutoSize = true;
-            this.buttonLogin.FlatAppearance.BorderSize = 0;
-            this.buttonLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonLogin.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonLogin.ForeColor = System.Drawing.Color.White;
-            this.buttonLogin.Location = new System.Drawing.Point(0, 132);
-            this.buttonLogin.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.buttonLogin.Name = "buttonLogin";
-            this.buttonLogin.Size = new System.Drawing.Size(264, 78);
-            this.buttonLogin.TabIndex = 3;
-            this.buttonLogin.Text = "Anmelden";
-            this.buttonLogin.UseVisualStyleBackColor = false;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(3, 115);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(258, 83);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Übersicht";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Century Gothic", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(3, 204);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(258, 83);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "1.Fach";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Century Gothic", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(3, 293);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(258, 83);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "2.Fach";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Century Gothic", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.Color.White;
+            this.button5.Location = new System.Drawing.Point(3, 382);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(258, 83);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "3.Fach";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Font = new System.Drawing.Font("Century Gothic", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.Color.White;
+            this.button6.Location = new System.Drawing.Point(3, 471);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(258, 83);
+            this.button6.TabIndex = 4;
+            this.button6.Text = "4.Fach";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.FlatAppearance.BorderSize = 0;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Font = new System.Drawing.Font("Century Gothic", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.ForeColor = System.Drawing.Color.White;
+            this.button7.Location = new System.Drawing.Point(3, 560);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(258, 83);
+            this.button7.TabIndex = 5;
+            this.button7.Text = "5.Fach";
+            this.button7.UseVisualStyleBackColor = true;
             // 
             // View
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1628, 887);
+            this.ClientSize = new System.Drawing.Size(1428, 769);
             this.Controls.Add(this.headerpanel);
             this.Controls.Add(this.sidepanel);
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "View";
             this.Text = "Lernkartentrainer";
             this.sidepanel.ResumeLayout(false);
-            this.sidepanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -91,7 +163,12 @@
 
         private System.Windows.Forms.Panel sidepanel;
         private System.Windows.Forms.Panel headerpanel;
-        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
     }
 }
 
